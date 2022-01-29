bc7c9d8c53bbca1b9800653efb7aef70d38b2f34
Derek Svart <37882224+Desvart@users.noreply.github.com>
2022-01-29 22:49:53 +0100

    Add files via upload

---
+++ initMaster.js
@@ -0,0 +1,23 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	ns.tail();
+    ns.clearLog();
+	ns.disableLog("ALL");
+
+	ns.print("STEP 0 - Release Jarvis");
+	ns.print("\t - NOT YET IMPLEMENTED");
+
+
+	ns.print("STEP 1 - Release the Hacknet daemon");
+
+	const host = "foodnstuff";
+	ns.run("/deployers/hacknetDeployer-v1.js", 1, host);
+
+	ns.print("\t - Hacknet daemon released");
+
+
+	ns.print("STEP 3 - Release the Factory daemon");
+	ns.print("\t - NOT YET IMPLEMENTED");
+
+	
+}
