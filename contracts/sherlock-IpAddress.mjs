feb7609b9e83d989e4a69e7455f09ecb97d1557e
Derek Svart <derek.svart@gmail.com>
2022-01-31 01:34:29 +0100

    #7 - Implement a first non operational draft of Watson & Sherlock.

---
+++ contracts/sherlock-IpAddress.mjs
@@ -0,0 +1,8 @@
+11410275226
+
+Note that an octet cannot begin with a '0' unless the number itself is actually 0. For example, '192.168.010.1' is not a valid IP.
+
+    Examples:
+
+25525511135 -> [255.255.11.135, 255.255.111.35]
+1938718066 -> [193.87.180.66]
---
+++ contracts/sherlock-totalWaysToSum.mjs
@@ -0,0 +1,6 @@
+3 + 1
+2 + 2
+2 + 1 + 1
+1 + 1 + 1 + 1
+
+How many different ways can the number 93 be written as a sum of at least two positive integers?
