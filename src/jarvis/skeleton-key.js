c3f9b01a66f4994128a700b1e1936519ffafb5a1
Derek Svart <derek.svart@gmail.com>
2022-02-25 20:52:05 +0100

    Refactor import path for jarvis module

--- src/jarvis/skeleton-key.js
+++ src/jarvis/skeleton-key.js
@@ -1,13 +1,12 @@
-import {Log} from '/helper.js';
+import { Log } from '../helpers/helper.js'
 
 export class SkeletonKey {
-
-	#KEYS = ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe'];
-    get availableKeysCount() { return this.#getAvailableKeys().length; }
-    #ns;
+	_KEYS = ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe'];
+  get availableKeysCount() { return this.#getAvailableKeys().length; }
+  _ns;
 
 	constructor(ns) {
-		this.#ns = ns;
+		this._ns = ns;
 	}
 
 
@@ -21,23 +20,23 @@ export class SkeletonKey {
 		let availableKeys = this.#getAvailableKeys();
 		for (let key of availableKeys)
 			switch (key) {
-				case this.#KEYS[0]: this.#ns.brutessh(target);  break;
-				case this.#KEYS[1]: this.#ns.ftpcrack(target);  break;
-				case this.#KEYS[2]: this.#ns.relaysmtp(target); break;
-				case this.#KEYS[3]: this.#ns.httpworm(target);  break;
-				case this.#KEYS[4]: this.#ns.sqlinject(target); break;
-				default: Log.error(this.#ns, `SKELETON_KEY - Key ${key} doesn't exists.`);
+				case this._KEYS[0]: this._ns.brutessh(target);  break;
+				case this._KEYS[1]: this._ns.ftpcrack(target);  break;
+				case this._KEYS[2]: this._ns.relaysmtp(target); break;
+				case this._KEYS[3]: this._ns.httpworm(target);  break;
+				case this._KEYS[4]: this._ns.sqlinject(target); break;
+				default: Log.error(this._ns, `SKELETON_KEY - Key ${key} doesn't exists.`);
 			}
 	}
 
 
 	#getAvailableKeys() {
-		return this.#KEYS.filter(key => this.#ns.fileExists(key, 'home'));
+		return this._KEYS.filter(key => this._ns.fileExists(key, 'home'));
 	}
 
 
 	#getRootAccess(target) {
-		this.#ns.nuke(target);
-        Log.success(this.#ns, `Node ${target} has been nuked successfuly.`)
+		this._ns.nuke(target);
+        Log.success(this._ns, `Node ${target} has been nuked successfuly.`)
 	}	
 }
