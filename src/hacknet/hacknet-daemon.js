1c2b25df649c34a0b9f5e6f25890f95f8834d32e
Derek Svart <derek.svart@gmail.com>
2022-03-06 00:10:11 +0100

    Cleanup & refactor to make it more lisible.

--- src/hacknet/hacknet-daemon.js
+++ src/hacknet/hacknet-daemon.js
@@ -1,128 +1,219 @@
+import {HACKNET_CONFIG, JARVIS_CONFIG, GLOBAL_CONFIG} from '/config/config.js';
+import {HacknetFarm} from '/hacknet/hacknet-farm.js';
+import {HacknetNode} from '/hacknet/hacknet-node.js';
 import {Log, initDaemon, formatMoney} from '/helpers/helper.js';
-import {HacknetConfig, JarvisConfig} 	from '/config/config.js';
-import {HacknetFarm} 									from '/hacknet/hacknet-farm.js';
-import {HacknetNode} 									from '/hacknet/hacknet-node.js';
 
 export async function main(ns) {
-	initDaemon(ns, '/hacknet/hacknet-daemon.js', HacknetConfig.displayTail);
-	
-	let hacknetDaemon = new HacknetDaemon(ns);
-	await hacknetDaemon.operate();
+    initDaemon(ns, HACKNET_CONFIG.DISPLAY_TAIL);
+    const hacknetDaemon = new HACKNET_CONFIG(ns);
+    await hacknetDaemon.operate();
 }
 
-
 export class HacknetDaemon {
-	_ns;
-	_cycleTime;
-	_harvestRatio;
-	location
-	queueId;
-	get _farm() { return new HacknetFarm(this._ns); }
-	
-	constructor(ns) {
-		this._ns 					 = ns;
-		this._cycleTime  	 = HacknetConfig.cycleTime;
-		this._harvestRatio = HacknetConfig.harvestRatio;
-		this._modulePath	 = HacknetConfig.modulePath;
-		this.location			 = HacknetConfig.location;
-		this.queueId			 = HacknetConfig.queueId;
-	}
+    #farm;
+    #ns;
+    
+    constructor(ns) {
+        this.#ns = ns;
+        this.#farm = new HacknetFarm(ns);
+    }
+    
+    static async deploy(ns, location) {
+        if (location !== 'home') {
+            await ns.scp(GLOBAL_CONFIG.CONFIG_FILE, location);
+            await ns.scp(HACKNET_CONFIG.DAEMON_FILE, location);
+            await ns.scp(HACKNET_CONFIG.FARM_FILE, location);
+            await ns.scp(HACKNET_CONFIG.NODE_FILE, location);
+            await ns.scp(GLOBAL_CONFIG.HELPER_FILE, location);
+        }
+    }
+    
+    static activate(ns, location) {
+        ns.nuke(location);
+        ns.exec(HACKNET_CONFIG.DAEMON_FILE, location, 1);
+    }
+    
+    async operate() {
+        
+        const [nodeId, componentName, cost] = this.#identifyCheapestComponentToUpgrade();
+        await this.#waitToHaveEnoughMoney(cost);
+        this.#upgradeHacknetFarm(nodeId, componentName);
+        
+        const etaBeforeNextUpgrade = this.#getEtaBeforeNextUpgrade();
+        await this.#waitUntilNextUpgradeOrSendWakeUpCallToJarvis(etaBeforeNextUpgrade);
+    }
+    
+    #identifyCheapestComponentToUpgrade() {
+        
+        let cheapestComponentToUpgrade = [];
+        let upgradeCost = Infinity;
+        
+        if (this.#farm.nodeCount !== 0) {
+            
+            const nodeWithCheapestLevelUpg = getNodeWithCheapestLevelUpgrade(this.#farm.nodeList);
+            const nodeWithCheapestRamUpg = getNodeWithCheapestRamUpgrade(this.#farm.nodeList);
+            const nodeWithCheapestCoreUpg = getNodeWithCheapestCoreUpgrade(this.#farm.nodeList);
+            
+            if (isLevelUpgTheCheapest(nodeWithCheapestLevelUpg, nodeWithCheapestRamUpg, nodeWithCheapestCoreUpg)) {
+                upgradeCost = nodeWithCheapestLevelUpg.levelUpgradeCost;
+                cheapestComponentToUpgrade = [
+                    nodeWithCheapestLevelUpg.id,
+                    HacknetNode.Component.LEVEL,
+                    Math.ceil(upgradeCost)];
+                
+            } else if (isRamUpgTheCheapest(nodeWithCheapestLevelUpg, nodeWithCheapestRamUpg, nodeWithCheapestCoreUpg)) {
+                upgradeCost = nodeWithCheapestRamUpg.ramUpgradeCost;
+                cheapestComponentToUpgrade = [
+                    nodeWithCheapestRamUpg.id,
+                    HacknetNode.Component.RAM,
+                    Math.ceil(upgradeCost)];
+                
+            } else if (isCoreUpgTheCheapest(nodeWithCheapestLevelUpg, nodeWithCheapestRamUpg, nodeWithCheapestCoreUpg)) {
+                upgradeCost = nodeWithCheapestCoreUpg.coreUpgradeCost;
+                cheapestComponentToUpgrade = [
+                    nodeWithCheapestCoreUpg.id,
+                    HacknetNode.Component.CORE,
+                    Math.ceil(upgradeCost)];
+            }
+        }
+        
+        if (this.#farm.newNodeCost < upgradeCost) {
+            cheapestComponentToUpgrade = [
+                this.#farm.nodeCount,
+                HacknetNode.Component.NODE,
+                Math.ceil(this.#farm.newNodeCost)];
+        }
+        
+        const costFormated = formatMoney(this.#ns, cost);
+        const msg = `HACKNET_DAEMON - Upgrade target: Node ${nodeId} - ${componentName} - Cost: ${costFormated}`;
+        Log.info(this.#ns, msg);
+        
+        return cheapestComponentToUpgrade;
+        
+        function getNodeWithCheapestLevelUpgrade(nodeList) {
+            return nodeList.reduce((prev, curr) => prev.levelUpgradeCost < curr.levelUpgradeCost ? prev : curr);
+        }
+        
+        function getNodeWithCheapestRamUpgrade(nodeList) {
+            return nodeList.reduce((prev, curr) => prev.ramUpgradeCost < curr.ramUpgradeCost ? prev : curr);
+        }
+        
+        function getNodeWithCheapestCoreUpgrade(nodeList) {
+            return nodeList.reduce((prev, curr) => prev.coreUpgradeCost < curr.coreUpgradeCost ? prev : curr);
+        }
+        
+        function isLevelUpgTheCheapest(
+            nodeWithCheapestLevelUpg, nodeWithCheapestRamUpg, nodeWithCheapestCoreUpg) {
+            return (nodeWithCheapestLevelUpg.levelUpgradeCost < nodeWithCheapestRamUpg.ramUpgradeCost &&
+                nodeWithCheapestLevelUpg.levelUpgradeCost < nodeWithCheapestCoreUpg.coreUpgradeCost);
+        }
+        
+        function isRamUpgTheCheapest(
+            nodeWithCheapestLevelUpg, nodeWithCheapestRamUpg, nodeWithCheapestCoreUpg) {
+            return (nodeWithCheapestRamUpg.ramUpgradeCost < nodeWithCheapestLevelUpg.levelUpgradeCost &&
+                nodeWithCheapestRamUpg.ramUpgradeCost < nodeWithCheapestCoreUpg.coreUpgradeCost);
+        }
+        
+        function isCoreUpgTheCheapest(
+            nodeWithCheapestLevelUpg, nodeWithCheapestRamUpg, nodeWithCheapestCoreUpg) {
+            return (nodeWithCheapestCoreUpg.coreUpgradeCost < nodeWithCheapestLevelUpg.levelUpgradeCost &&
+                nodeWithCheapestCoreUpg.coreUpgradeCost < nodeWithCheapestRamUpg.ramUpgradeCost);
+        }
+        
+    }
+    
+    async;
+    
+    #waitToHaveEnoughMoney(cost); {
+    const;
+    wealth = this.#ns.getPlayer().money;
+    
+    while(wealth
 
+<=
+    cost;
+) {
+    
+    const;
+    wealthFormated = formatMoney(this.#ns, wealth);
+    const;
+    costFormated = formatMoney(this.#ns, cost);
+    const;
+    msg = `HACKNET_DAEMON - Not enough money! Cost: ${costFormated}, available: ${wealthFormated}`;
+    Log;
+.
+    
+    warn(
+    
+    this;
+.
+    #ns;
+,
+    msg;
+)
+    ;
+    
+    await;
+    this;
+.
+    #ns;
+.
+    
+    sleep(HACKNET_CONFIG
 
-	static async deploy(ns, location) {
-		if (location !== 'home') {
-			await ns.scp('/helpers/helper.js', 				location);
-			await ns.scp(HacknetConfig.modulePath + 'hacknet-daemon.js', location);
-			await ns.scp(HacknetConfig.modulePath + 'hacknet-farm.js', 	location);
-			await ns.scp(HacknetConfig.modulePath + 'hacknet-node.js', 	location);
-			await ns.scp('/config/config.js', 					location);
-		}
-	}
+.
+    CYCLE_TIME;
+    
+    * 10
 
-	static activate(ns, location) {
-		ns.nuke(location);
-		ns.exec(HacknetConfig.modulePath + 'hacknet-daemon.js', location, 1);
-	}
+)
+    ;
+}
+}
 
+#upgradeHacknetFarm(nodeId, componentName);
+{
+    if (componentName === HacknetNode.Component.NODE) {
+        this.#farm.buyNewNode();
+    } else {
+        this.#farm.nodeList[nodeId].upgrade(componentName);
+    }
+}
 
+#getEtaBeforeNextUpgrade();
+{
+    const [nodeIdNext, componentNameNext, costNext] = this.#identifyCheapestComponentToUpgrade();
+    const timeToRoI = costNext / this.#farm.productionRate; //s
+    let etaBeforeNextUpgrade = Math.ceil(timeToRoI / HACKNET_CONFIG.HARVEST_RATIO) * 1000; //ms
+    
+    if (componentNameNext === HacknetNode.Component.NODE) {
+        Log.info(this.#ns,
+            `HACKNET_DAEMON - Next upgrade: New node ${nodeIdNext} in ${etaBeforeNextUpgrade} s.`);
+        
+    } else {
+        const upgradeNext = this.#farm.nodeList[nodeIdNext][componentNameNext] + 1;
+        const msg = `HACKNET_DAEMON - Next upgrade: Node ${nodeIdNext} - ${componentNameNext} -> ${upgradeNext} in
+                ${etaBeforeNextUpgrade / 1000} s.`;
+        Log.info(this.#ns, msg);
+    }
+    
+    return etaBeforeNextUpgrade;
+}
 
-	async operate() {
-
-		// check which component should be upgraded (least expensive)
-		let [nodeId, componentName, cost] = this.#identifyCheapestComponentToUpgrade();
-		Log.info(this._ns, `HACKNET_DAEMON - Upgrade target: Node ${nodeId} - ${componentName} - Cost: ${formatMoney(this._ns, cost)}`);
-
-		// check available funds
-		while (this._ns.getPlayer().money <= cost) {
-			let availableFund = formatMoney(this._ns, this._ns.getPlayer().money);
-			Log.warn(this._ns, `HACKNET_DAEMON - Not enough money! Cost: ${formatMoney(this._ns, cost)}, available: ${availableFund}`);
-			await this._ns.sleep(this._cycleTime * 10);
-		}
-
-		// upgrade
-		if (componentName === HacknetNode.Component.NODE) {
-			this._farm.buyNewNode();
-		} else {
-			this._farm.nodeList[nodeId].upgrade(componentName);
-		}
-
-		// check which next element should be upgraded (least expensive)
-		let [nodeIdNext, componentNameNext, costNext] = this.#identifyCheapestComponentToUpgrade();
-
-		// compute time before next upgrade
-		let timeToRoI 				= costNext / this._farm.production; //s
-		let timeBeforeNextUpgrade 	= Math.ceil(timeToRoI / this._harvestRatio) * 1000; //ms
-
-		if (componentNameNext === HacknetNode.Component.NODE) {
-			Log.info(this._ns, `HACKNET_DAEMON - Next upgrade: New node ${nodeIdNext} in ${timeBeforeNextUpgrade} s.`);
-		} else {
-			let upgradeNext = this._farm.nodeList[nodeIdNext][componentNameNext] + 1;
-			Log.info(this._ns, `HACKNET_DAEMON - Next upgrade: Node ${nodeIdNext} - ${componentNameNext} -> ${upgradeNext} in ${timeBeforeNextUpgrade / 1000} s.`);
-		}
-
-		// if less than Jarvis loop, wait, else send Jarvis the wake-up timestamp
-		if (timeBeforeNextUpgrade < JarvisConfig.cycleTime) {
-			await this._ns.sleep(timeBeforeNextUpgrade);
-			await this.operate();
-		} else {
-			await this._ns.writePort(this.queueId, Date.now() + timeBeforeNextUpgrade);
-			Log.info(this._ns, `HACKNET_DAEMON - Quit Hacknet Daemon. Jarvis should relaunch Hacknet Daemon in ${timeBeforeNextUpgrade / 1000} s.`);
-		}
-	}
-
-
-	#identifyCheapestComponentToUpgrade() {
-
-		let cheapestComponentToUpgrade = [];
-		let upgradeCost = Infinity;
-		
-		if (this._farm.nodeCount !== 0) {
-			let nodeWithCheapestLevelUpgrade = this._farm.nodeList.reduce((prev, curr) => prev.levelUpgradeCost < curr.levelUpgradeCost ? prev : curr);
-			let nodeWithCheapestRamUpgrade 	 = this._farm.nodeList.reduce((prev, curr) => prev.ramUpgradeCost   < curr.ramUpgradeCost   ? prev : curr);
-			let nodeWithCheapestCoreUpgrade  = this._farm.nodeList.reduce((prev, curr) => prev.coreUpgradeCost  < curr.coreUpgradeCost  ? prev : curr);
-			
-			
-			if (nodeWithCheapestLevelUpgrade.levelUpgradeCost < nodeWithCheapestRamUpgrade.ramUpgradeCost && 
-				nodeWithCheapestLevelUpgrade.levelUpgradeCost < nodeWithCheapestCoreUpgrade.coreUpgradeCost) {
-				upgradeCost = nodeWithCheapestLevelUpgrade.levelUpgradeCost;
-				cheapestComponentToUpgrade = [nodeWithCheapestLevelUpgrade.id, HacknetNode.Component.LEVEL, Math.ceil(upgradeCost)];
-
-			} else if (nodeWithCheapestRamUpgrade.ramUpgradeCost < nodeWithCheapestLevelUpgrade.levelUpgradeCost && 
-					   nodeWithCheapestRamUpgrade.ramUpgradeCost < nodeWithCheapestCoreUpgrade.coreUpgradeCost) {
-				upgradeCost = nodeWithCheapestRamUpgrade.ramUpgradeCost;
-				cheapestComponentToUpgrade = [nodeWithCheapestRamUpgrade.id, HacknetNode.Component.RAM, Math.ceil(upgradeCost)];
-
-			} else if (nodeWithCheapestCoreUpgrade.coreUpgradeCost < nodeWithCheapestLevelUpgrade.levelUpgradeCost && 
-					   nodeWithCheapestCoreUpgrade.coreUpgradeCost < nodeWithCheapestRamUpgrade.ramUpgradeCost) {
-				upgradeCost = nodeWithCheapestCoreUpgrade.coreUpgradeCost;
-				cheapestComponentToUpgrade = [nodeWithCheapestCoreUpgrade.id, HacknetNode.Component.CORE, Math.ceil(upgradeCost)];
-			}
-		}
-
-		if (this._farm.newNodeCost < upgradeCost) {
-			cheapestComponentToUpgrade = [this._farm.nodeCount, HacknetNode.Component.NODE, Math.ceil(this._farm.newNodeCost)];
-		}
-
-		return cheapestComponentToUpgrade;
-	}
+async;
+#waitUntilNextUpgradeOrSendWakeUpCallToJarvis(etaBeforeNextUpgrade);
+{
+    // if less than Jarvis cycle time, wait, else send Jarvis a wake-up call
+    
+    if (etaBeforeNextUpgrade < JARVIS_CONFIG.CYCLE_TIME) {
+        await this.#ns.sleep(etaBeforeNextUpgrade);
+        await this.operate();
+        
+    } else {
+        await this.#ns.writePort(HACKNET_CONFIG.QUEUE_ID, Date.now() + etaBeforeNextUpgrade);
+        const msg = `HACKNET_DAEMON - Quit Hacknet Daemon. Jarvis should relaunch Hacknet Daemon in
+                ${etaBeforeNextUpgrade / 1000} s.`;
+        Log.info(this.#ns, msg);
+    }
+}
 }
