e334bcabdee936578c865662d60480eb2afbaf9b
Derek Svart <derek.svart@gmail.com>
2022-03-05 18:55:32 +0100

    Cleanup

--- src/hacknet/hacknet-farm.js
+++ src/hacknet/hacknet-farm.js
@@ -1,47 +1,48 @@
-import {Log} 				 from '/helpers/helper.js';
+import {Log} from '/helpers/helper.js';
 import {HacknetNode} from '/hacknet/hacknet-node.js';
 
 export class HacknetFarm {
-	_ns;
-	get MAX_NODE_COUNT()  { return this._ns.hacknet.maxNumNodes(); }
-	get nodeCount()  	  	{ return this._ns.hacknet.numNodes(); }
-	get newNodeCost() 	  { return this._ns.hacknet.getPurchaseNodeCost(); }
-	get nodeList() 	 	  	{ return this.#getNodeList(); 		}
-	get production()	  	{ return this.#getProduction(); 		}
-	get totalProduction() { return this.#getTotalProduction(); 	}
-	//get investment() 	  { return this.#getInvestment(); 		}
-
-
-	constructor(ns) {
-		//console.debug(`Hook - ${ns.getScriptName()} - Hacknet Farm`);
-		this._ns = ns;
-	}
-
-
-	#getNodeList() {
-		let nodeList = [];
-		for (let i = 0; i < this.nodeCount; i++) {
-			let node = new HacknetNode(this._ns, i);
-			nodeList.push(node);
-		}
-		return nodeList;
-	}
-
-	#getProduction() {
-		let prod = this.nodeList.reduce((prev, curr) => prev + curr.production, 0);
-		return prod;
-	}
-
-	#getTotalProduction() {
-		return this.nodeList.reduce((prev, curr) => prev.totalProduction + curr.totalProduction);
-	}
-
-	#getInvestment() {
-		return this.nodeList.reduce((prev, curr) => prev.investment + curr.investment);
-	}
-
-	buyNewNode() {
-		let nodeId = this._ns.hacknet.purchaseNode();
-		Log.success(this._ns, `HACKNET_FARM - New node ${nodeId} bought.`);
-	}
+    MAX_NODE_COUNT;
+    #ns;
+    
+    get nodeCount() { return this.#ns.hacknet.numNodes(); }
+    
+    get newNodeCost() { return this.#ns.hacknet.getPurchaseNodeCost(); }
+    
+    get nodeList() { return this.#getNodeList(); }
+    
+    get productionRate() { return this.#getProductionRate(); }
+    
+    get totalProduction() { return this.#getTotalProduction(); }
+    
+    constructor(ns) {
+        this.#ns = ns;
+        this.MAX_NODE_COUNT = this.#ns.hacknet.maxNumNodes();
+    }
+    
+    #getNodeList() {
+        let nodeList = [];
+        for (let i = 0; i < this.nodeCount; i++) {
+            const node = new HacknetNode(this.#ns, i);
+            nodeList.push(node);
+        }
+        return nodeList;
+    }
+    
+    #getProductionRate() {
+        return this.nodeList.reduce((prev, curr) => prev + curr.productionRate, 0);
+    }
+    
+    #getTotalProduction() {
+        return this.nodeList.reduce((prev, curr) => prev.totalProduction + curr.totalProduction);
+    }
+    
+    buyNewNode() {
+        if (this.nodeCount < this.MAX_NODE_COUNT) {
+            const nodeId = this.#ns.hacknet.purchaseNode();
+            Log.success(this.#ns, `HACKNET_FARM - New node ${nodeId} bought.`);
+        } else {
+            Log.info(this.#ns, `HACKNET_FARM - Max number of nodes (${this.MAX_NODE_COUNT}) already bought.`);
+        }
+    }
 }
